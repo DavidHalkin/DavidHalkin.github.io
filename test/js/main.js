51338818c4b9d21de17215eae462183274ba9416
David <northfire@mail.ru>
2017-07-02 16:33:20 +0300

    test 1 add anim

---
+++ test/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+	jcf.replaceAll();
+});
+// $(document).ready(function() {
+	
+// 	setTimeout(function(){
+// 		$('body').addClass('loaded');
+// 		 new WOW().init();
+// 	}, 200);
+	
+// });
